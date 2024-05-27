b9b57bb4a53b213d9f4f4c09b2d728a7ef3d582c
narwa <narwandaru.nara@gmail.com>
2024-05-27 13:33:46 +0700

    feat: routing for CRUD and custom methods

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,17 +8,23 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'HomeController::index');
 
 
-/**
- * Routes to the todo list page
- */
+// Routes to the todo list page
 $routes->get('/todos-page', 'TodosController::index');
 
-/**
- * Route to POST /todos to create a new todo
- */
+// Route to GET /todos to fetch all todos
+$routes->get('/todos','TodosController::getTodos');
+
+// Route to POST /todos to create a new todo
 $routes->post('/todos', 'TodosController::create');
 
-/**
- * Route to GET /todos to fetch all todos
- */
-$routes->get('/todos','TodosController::getTodos');
+// Route to DELETE /todos/{id} to delete a todo
+$routes->delete('/todos/(:num)', 'TodosController::delete');
+
+// Route to PUT /todos/{id}/title to edit a todo's title
+$routes->put('/todos/(:num)/title', 'TodosController::editTitle');
+
+// (currently unused) Route to PUT /todos/{id}/status to edit a todo's status
+$routes->put('/todos/(:num)/status', 'TodosController::editStatus');
+
+// Route to PUT /todos/{id}/toggle-status to toggle a todo's status
+$routes->put('/todos/(:num)/toggle-status', 'TodosController::toggleStatus');
