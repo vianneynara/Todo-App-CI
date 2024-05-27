cbbb05b91ec15210dac19afff1e089da9d1f79b0
narwa <narwandaru.nara@gmail.com>
2024-05-27 17:21:10 +0700

    refactor: refactor todos controller to todo controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,22 +9,22 @@ $routes->get('/', 'HomeController::index');
 
 
 // Routes to the todo list page
-$routes->get('/todos-page', 'TodosController::index');
+$routes->get('/todos-page', 'TodoController::index');
 
 // Route to GET /todos to fetch all todos
-$routes->get('/todos','TodosController::getTodos');
+$routes->get('/todos','TodoController::getTodos');
 
 // Route to POST /todos to create a new todo
-$routes->post('/todos', 'TodosController::create');
+$routes->post('/todos', 'TodoController::create');
 
 // Route to DELETE /todos/{id} to delete a todo
-$routes->delete('/todos/(:num)', 'TodosController::delete');
+$routes->delete('/todos/(:num)', 'TodoController::delete');
 
 // Route to PUT /todos/{id}/title to edit a todo's title
-$routes->put('/todos/(:num)/title', 'TodosController::editTitle');
+$routes->put('/todos/(:num)/title', 'TodoController::editTitle');
 
 // (currently unused) Route to PUT /todos/{id}/status to edit a todo's status
-$routes->put('/todos/(:num)/status', 'TodosController::editStatus');
+$routes->put('/todos/(:num)/status', 'TodoController::editStatus');
 
 // Route to PUT /todos/{id}/toggle-status to toggle a todo's status
-$routes->put('/todos/(:num)/toggle-status', 'TodosController::toggleStatus');
+$routes->put('/todos/(:num)/toggle-status', 'TodoController::toggleStatus');
