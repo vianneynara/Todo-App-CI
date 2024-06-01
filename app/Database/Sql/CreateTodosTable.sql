f1363654d5068777022746f87074fa53e8f0de3d
narwa <narwandaru.nara@gmail.com>
2024-06-01 22:45:57 +0700

    feat: added user_id field as FK

---
+++ app/Database/Sql/CreateTodosTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todos (
+    todo_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    isDone TINYINT(1) DEFAULT 0,
+    user_id INT(5) UNSIGNED FOREIGN KEY REFERENCES users (user_id)
+);
